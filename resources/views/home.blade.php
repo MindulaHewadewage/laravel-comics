fb6b156406d6125a50d973022f522095b50cf9d9
MindulaHewadewage <mindufernando@gmail.com>
2023-02-23 16:30:32 +0100

    rotte

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <header>
+        <section id="top-header">
+            <div class="bg-primary">
+                <div class="container">
+                    <ul>
+                        <li><a href="#">DC POWER VISA </a></li>
+                        <li><a href="#">ADDITIONAL DC SITES</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section section id="bottom-header">
+        <div class="container">
+            <div class="logo">
+                <a href=""><img src="" alt=""></a>
+            </div>
+            <div class="menu">
+                <li><a href=""></a></li>
+            </div>
+            <div class="search"></div>
+        </div>
+        </section >
+    </header>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// rotta per la home
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+// rotta per la characters
+Route::get('/characters', function () {
+    return view('characters');
+})->name('characters');
+
+// rotta per la comics
+Route::get('/comics', function () {
+    return view('comics');
+})->name('comics');
+
+// rotta per la movies
+Route::get('/movies', function () {
+    return view('movies');
+})->name('movies');
+
+// rotta per la tv
+Route::get('/tv', function () {
+    return view('tv');
+})->name('tv');
+
+// rotta per la games
+Route::get('/games', function () {
+    return view('games');
+})->name('games');
+
+// rotta per la collectibles
+Route::get('/collectibles', function () {
+    return view('collectibles');
+})->name('collectibles');
+
+// rotta per la videos
+Route::get('/videos', function () {
+    return view('videos');
+})->name('videos');
+
+// rotta per la fans
+Route::get('/fans', function () {
+    return view('fans');
+})->name('fans');
+
+// rotta per la news
+Route::get('/', function () {
+    return view('news');
+})->name('news');
+
+// rotta per la shop
+Route::get('/', function () {
+    return view('shop');
+})->name('shop');
