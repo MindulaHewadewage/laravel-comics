100ee80e7bfdd9712bb9206c9e4d4cbcce7b4ee5
MindulaHewadewage <mindufernando@gmail.com>
2023-02-26 20:14:45 +0100

    buy-sectioin img fixed

--- resources/views/includes/buy-section.blade.php
+++ resources/views/includes/buy-section.blade.php
@@ -1,24 +1,24 @@
 <section class="buy-section-bg">
     <ul class="buy-list xs-container">
         <li>
-            <a href="#"><img src="{{asset('img/buy-comics-digital-comics.png')}}" alt=""></a>
-            <h6>DIGITAL COMICS</h6>
+            <a href="#"><img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt=""></a>
+            <h5>DIGITAL COMICS</h5>
         </li>
         <li>
-            <a href="#"><img src="{{asset('img/buy-comics-digital-comics.png')}}" alt=""></a>
-            <h6>DC MERCHANDISE</h6>
+            <a href="#"><img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt=""></a>
+            <h5>DC MERCHANDISE</h5>
         </li>
         <li>
-            <a href="#"><img src="{{asset('img/buy-comics-digital-comics.png')}}" alt=""></a>
-            <h6>SUBSCRIPTION</h6>
+            <a href="#"><img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt=""></a>
+            <h5>SUBSCRIPTION</h5>
         </li>
         <li>
-            <a href="#"><img src="{{asset('img/buy-comics-digital-comics.png')}}" alt=""></a>
-            <h6>COMIC SHOP LOCATION</h6>
+            <a href="#"><img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt=""></a>
+            <h5>COMIC SHOP LOCATION</h5>
         </li>
         <li>
-            <a href="#"><img src="{{asset('img/buy-comics-digital-comics.png')}}" alt=""></a>
-            <h6>DC POWER VISA</h6>
+            <a href="#"><img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt=""></a>
+            <h5>DC POWER VISA</h5>
         </li>
 
     </ul>
