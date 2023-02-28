f73363fbb33a57071c6052c1d89fc8deaac9d782
MindulaHewadewage <mindufernando@gmail.com>
2023-02-28 18:14:36 +0100

    jumbo fixed+

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -4,9 +4,7 @@
 
 
 @section('content')
-<div class="comics-jumbo">
-    <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
-</div>
+<div class="comics-jumbo"></div>
 <div class="comics-card-section">
 <div class="comics-card-deck container">
     @foreach($comics as $comic)
